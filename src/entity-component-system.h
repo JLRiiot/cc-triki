41fff39176ef1cf6bd4ee093d72d7ae0ea33e4b8
Jose Montana <ing.jmontana@gmail.com>
2024-06-01 12:39:16 -0500

    chore(Basic ECS): created basic ecs lib

--- src/entity-component-system.h
+++ src/entity-component-system.h
@@ -4,6 +4,7 @@
 #include <bitset>
 #include <queue>
 #include <cassert>
+#include <set>
 
 #ifdef _WIN32
 #define ECS_EXPORT __declspec(dllexport)
@@ -22,8 +23,11 @@ ECS_EXPORT using Signature = std::bitset<MAX_COMPONENTS>;
 class ECS_EXPORT EntityManager
 {
 private:
+    // Queue of unused entity IDs
     std::queue<Entity> mAvailableEntities{};
+    // Array of signatures where the index corresponds to the entity ID
     std::array<Signature, MAX_ENTITIES> mSignatures{};
+    // Total living entities - used to keep limits on how many exist
     std::uint32_t mLivingEntityCount{};
 
 public:
@@ -69,5 +73,324 @@ public:
         assert(entity < MAX_ENTITIES && "Entity is out of range.");
 
         return mSignatures[entity];
+    }
+};
+
+class ECS_EXPORT IComponentArray
+{
+public:
+    virtual ~IComponentArray() = default;
+    virtual void EntityDestroyed(Entity entity) = 0;
+};
+
+template <typename T>
+class ECS_EXPORT ComponentArray : public IComponentArray
+{
+private:
+    // The dense array of components, it is also called packed array
+    // because it is a contiguous block of memory
+    // meaning that the components are stored next to each other in memory
+    // and there are no gaps between them.
+    std::array<T, MAX_ENTITIES> mComponentArray;
+    // Map from an entity ID to an array index.
+    std::unordered_map<Entity, std::size_t> mEntityToIndexMap;
+    // Map from an array index to an entity ID.
+    std::unordered_map<std::size_t, Entity> mIndexToEntityMap;
+
+    // Total size of valid entries in the array.
+    std::size_t mSize{};
+
+public:
+    /**
+     * @brief Inserts the component at the end of the array, and updates the maps.
+     *
+     * @param entity The entity ID to associate with the component.
+     * @param component The component to add to the array.
+     *
+     * @note This method will assert if the entity already has a component in the array.
+     */
+    void InsertData(Entity entity, T component)
+    {
+        assert(mEntityToIndexMap.find(entity) == mEntityToIndexMap.end() && "Component added to same entity more than once.");
+
+        std::size_t newIndex = mSize;
+        mEntityToIndexMap[entity] = newIndex;
+        mIndexToEntityMap[newIndex] = entity;
+        ++mSize;
+    }
+
+    /**
+     * @brief Removes a component from the array, and updates the maps.
+     *
+     * @param entity The entity ID to remove from the array.
+     *
+     * The remove process will swap the last element with the element to remove,
+     * and update the maps to point to the new location of the moved element.
+     *
+     * @note This method will assert if the entity does not have a component in the array.
+     */
+    void RemoveData(Entity entity)
+    {
+        assert(mEntityToIndexMap.find(entity) != mEntityToIndexMap.end() && "Removing non-existent component.");
+
+        // First find the index of the component that will be removed
+        std::size_t indexOfRemovedEntity = mEntityToIndexMap[entity];
+        std::size_t indexOfLastElement = mSize - 1;
+        // Now swap the last element with the element to remove
+        mComponentArray[indexOfRemovedEntity] = mComponentArray[indexOfLastElement];
+
+        // Next update the maps to point to the new locations
+        Entity entityOfLastElement = mIndexToEntityMap[indexOfLastElement];
+        mEntityToIndexMap[entityOfLastElement] = indexOfRemovedEntity;
+        mIndexToEntityMap[indexOfRemovedEntity] = entityOfLastElement;
+
+        // Finally remove the entity, remove the index map, and reduce the size
+        mEntityToIndexMap.erase(entity);
+        mIndexToEntityMap.erase(indexOfLastElement);
+        --mSize;
+    }
+
+    T &GetData(Entity entity)
+    {
+        assert(mEntityToIndexMap.find(entity) != mEntityToIndexMap.end() && "Retrieving non-existent component.");
+        std::size_t indexOfEntity = mEntityToIndexMap[entity];
+
+        return mComponentArray[indexOfEntity];
+    }
+
+    void EntityDestroyed(Entity entity) override
+    {
+        if (mEntityToIndexMap.find(entity) != mEntityToIndexMap.end())
+        {
+            RemoveData(entity);
+        }
+    }
+};
+
+class ECS_EXPORT ComponentManager
+{
+private:
+    std::unordered_map<const char *, ComponentType> mComponentTypes{};
+    std::unordered_map<const char *, std::shared_ptr<IComponentArray>> mComponentArrays{};
+    ComponentType mNextComponentType{};
+
+    template <typename T>
+    std::shared_ptr<ComponentArray<T>> GetComponentArray()
+    {
+        const char *typeName = typeid(T).name();
+
+        assert(mComponentTypes.find(typeName) != mComponentTypes.end() && "Component not registered before use.");
+
+        return std::static_pointer_cast<ComponentArray<T>>(mComponentArrays[typeName]);
+    }
+
+public:
+    template <typename T>
+    void RegisterComponent()
+    {
+        const char *typeName = typeid(T).name();
+
+        assert(mComponentTypes.find(typeName) == mComponentTypes.end() && "Registering component type more than once.");
+
+        // Add this component type to the component type map
+        mComponentTypes.insert({typeName, mNextComponentType});
+
+        // Create a ComponentArray pointer and add it to the component arrays map
+        mComponentArrays.insert({typeName, std::make_shared<ComponentArray<T>>()});
+
+        // Increment the value so that the next component registered will be different
+        ++mNextComponentType;
+    }
+
+    template <typename T>
+    ComponentType GetComponentType()
+    {
+        const char *typeName = typeid(T).name();
+
+        assert(mComponentTypes.find(typeName) != mComponentTypes.end() && "Component not registered before use.");
+
+        return mComponentTypes[typeName];
+    }
+
+    template <typename T>
+    void AddComponent(Entity entity, T component)
+    {
+        GetComponentArray<T>()->InsertData(entity, component);
+    }
+
+    template <typename T>
+    void RemoveComponent(Entity entity)
+    {
+        GetComponentArray<T>()->RemoveData(entity);
+    }
+
+    template <typename T>
+    T &GetComponent(Entity entity)
+    {
+        return GetComponentArray<T>()->GetData(entity);
+    }
+
+    void EntityDestroyed(Entity entity)
+    {
+        for (auto const &pair : mComponentArrays)
+        {
+            auto const &componentArray = pair.second;
+
+            componentArray->EntityDestroyed(entity);
+        }
+    }
+};
+
+class ECS_EXPORT System
+{
+public:
+    std::set<Entity> mEntities{};
+};
+
+class ECS_EXPORT SystemManager
+{
+private:
+    std::unordered_map<const char *, std::shared_ptr<System>> mSystems{};
+    std::unordered_map<const char *, Signature> mSignatures{};
+
+public:
+    template <typename T>
+    std::shared_ptr<T> RegisterSystem()
+    {
+        const char *typeName = typeid(T).name();
+
+        assert(mSystems.find(typeName) == mSystems.end() && "Registering system more than once.");
+
+        auto system = std::make_shared<T>();
+        mSystems.insert({typeName, system});
+
+        return system;
+    }
+
+    template <typename T>
+    void SetSignature(Signature signature)
+    {
+        const char *typeName = typeid(T).name();
+
+        assert(mSystems.find(typeName) != mSystems.end() && "System used before registered.");
+
+        mSignatures.insert({typeName, signature});
+    }
+
+    void EntityDestroyed(Entity entity)
+    {
+        for (auto pair : mSystems)
+        {
+            auto const &system = pair.second;
+
+            system->mEntities.erase(entity);
+        }
+    }
+
+    void EntitySignatureChanged(Entity entity, Signature newSignature)
+    {
+        for (auto pair : mSystems)
+        {
+            auto const &type = pair.first;
+            auto const &system = pair.second;
+            auto const &systemSignature = mSignatures[type];
+
+            if ((newSignature & systemSignature) == systemSignature)
+            {
+                system->mEntities.insert(entity);
+            }
+            else
+            {
+                system->mEntities.erase(entity);
+            }
+        }
+    }
+};
+
+class Coordinator
+{
+private:
+    std::unique_ptr<ComponentManager> mComponentManager;
+    std::unique_ptr<EntityManager> mEntityManager;
+    std::unique_ptr<SystemManager> mSystemManager;
+
+public:
+    void Init()
+    {
+        // Create pointers to each manager
+        mComponentManager = std::make_unique<ComponentManager>();
+        mEntityManager = std::make_unique<EntityManager>();
+        mSystemManager = std::make_unique<SystemManager>();
+    }
+
+    // Entity methods
+    Entity CreateEntity()
+    {
+        return mEntityManager->CreateEntity();
+    }
+
+    void DestroyEntity(Entity entity)
+    {
+        mEntityManager->DestroyEntity(entity);
+
+        mComponentManager->EntityDestroyed(entity);
+
+        mSystemManager->EntityDestroyed(entity);
+    }
+
+    // Component methods
+    template <typename T>
+    void RegisterComponent()
+    {
+        mComponentManager->RegisterComponent<T>();
+    }
+
+    template <typename T>
+    void AddComponent(Entity entity, T component)
+    {
+        mComponentManager->AddComponent<T>(entity, component);
+
+        auto signature = mEntityManager->GetSignature(entity);
+        signature.set(mComponentManager->GetComponentType<T>(), true);
+        mEntityManager->SetSignature(entity, signature);
+
+        mSystemManager->EntitySignatureChanged(entity, signature);
+    }
+
+    template <typename T>
+    void RemoveComponent(Entity entity)
+    {
+        mComponentManager->RemoveComponent<T>(entity);
+
+        auto signature = mEntityManager->GetSignature(entity);
+        signature.set(mComponentManager->GetComponentType<T>(), false);
+        mEntityManager->SetSignature(entity, signature);
+
+        mSystemManager->EntitySignatureChanged(entity, signature);
+    }
+
+    template <typename T>
+    T &GetComponent(Entity entity)
+    {
+        return mComponentManager->GetComponent<T>(entity);
+    }
+
+    template <typename T>
+    ComponentType GetComponentType()
+    {
+        return mComponentManager->GetComponentType<T>();
+    }
+
+    // System methods
+    template <typename T>
+    std::shared_ptr<T> RegisterSystem()
+    {
+        return mSystemManager->RegisterSystem<T>();
+    }
+
+    template <typename T>
+    void SetSystemSignature(Signature signature)
+    {
+        mSystemManager->SetSignature<T>(signature);
     }
 };
--- src/triqui.cpp
+++ src/triqui.cpp
@@ -1,120 +1,119 @@
 #include <iostream>
 #include "triqui.h"
 
-
-
-void triqui(){
-    
-
-    #ifdef NDEBUG
+void triqui()
+{
+#ifdef NDEBUG
     std::cout << "triqui/0.1: Hello World Release!\n";
-    #else
+#else
     std::cout << "triqui/0.1: Hello World Debug!\n";
-    #endif
+#endif
 
-    // ARCHITECTURES
-    #ifdef _M_X64
+// ARCHITECTURES
+#ifdef _M_X64
     std::cout << "  triqui/0.1: _M_X64 defined\n";
-    #endif
+#endif
 
-    #ifdef _M_IX86
+#ifdef _M_IX86
     std::cout << "  triqui/0.1: _M_IX86 defined\n";
-    #endif
+#endif
 
-    #ifdef _M_ARM64
+#ifdef _M_ARM64
     std::cout << "  triqui/0.1: _M_ARM64 defined\n";
-    #endif
+#endif
 
-    #if __i386__
+#if __i386__
     std::cout << "  triqui/0.1: __i386__ defined\n";
-    #endif
+#endif
 
-    #if __x86_64__
+#if __x86_64__
     std::cout << "  triqui/0.1: __x86_64__ defined\n";
-    #endif
+#endif
 
-    #if __aarch64__
+#if __aarch64__
     std::cout << "  triqui/0.1: __aarch64__ defined\n";
-    #endif
+#endif
 
-    // Libstdc++
-    #if defined _GLIBCXX_USE_CXX11_ABI
-    std::cout << "  triqui/0.1: _GLIBCXX_USE_CXX11_ABI "<< _GLIBCXX_USE_CXX11_ABI << "\n";
-    #endif
+// Libstdc++
+#if defined _GLIBCXX_USE_CXX11_ABI
+    std::cout << "  triqui/0.1: _GLIBCXX_USE_CXX11_ABI " << _GLIBCXX_USE_CXX11_ABI << "\n";
+#endif
 
-    // MSVC runtime
-    #if defined(_DEBUG)
-        #if defined(_MT) && defined(_DLL)
-        std::cout << "  triqui/0.1: MSVC runtime: MultiThreadedDebugDLL\n";
-        #elif defined(_MT)
-        std::cout << "  triqui/0.1: MSVC runtime: MultiThreadedDebug\n";
-        #endif
-    #else
-        #if defined(_MT) && defined(_DLL)
-        std::cout << "  triqui/0.1: MSVC runtime: MultiThreadedDLL\n";
-        #elif defined(_MT)
-        std::cout << "  triqui/0.1: MSVC runtime: MultiThreaded\n";
-        #endif
-    #endif
+// MSVC runtime
+#if defined(_DEBUG)
+#if defined(_MT) && defined(_DLL)
+    std::cout << "  triqui/0.1: MSVC runtime: MultiThreadedDebugDLL\n";
+#elif defined(_MT)
+    std::cout << "  triqui/0.1: MSVC runtime: MultiThreadedDebug\n";
+#endif
+#else
+#if defined(_MT) && defined(_DLL)
+    std::cout << "  triqui/0.1: MSVC runtime: MultiThreadedDLL\n";
+#elif defined(_MT)
+    std::cout << "  triqui/0.1: MSVC runtime: MultiThreaded\n";
+#endif
+#endif
 
-    // COMPILER VERSIONS
-    #if _MSC_VER
-    std::cout << "  triqui/0.1: _MSC_VER" << _MSC_VER<< "\n";
-    #endif
+// COMPILER VERSIONS
+#if _MSC_VER
+    std::cout << "  triqui/0.1: _MSC_VER" << _MSC_VER << "\n";
+#endif
 
-    #if _MSVC_LANG
-    std::cout << "  triqui/0.1: _MSVC_LANG" << _MSVC_LANG<< "\n";
-    #endif
+#if _MSVC_LANG
+    std::cout << "  triqui/0.1: _MSVC_LANG" << _MSVC_LANG << "\n";
+#endif
 
-    #if __cplusplus
-    std::cout << "  triqui/0.1: __cplusplus" << __cplusplus<< "\n";
-    #endif
+#if __cplusplus
+    std::cout << "  triqui/0.1: __cplusplus" << __cplusplus << "\n";
+#endif
 
-    #if __INTEL_COMPILER
-    std::cout << "  triqui/0.1: __INTEL_COMPILER" << __INTEL_COMPILER<< "\n";
-    #endif
+#if __INTEL_COMPILER
+    std::cout << "  triqui/0.1: __INTEL_COMPILER" << __INTEL_COMPILER << "\n";
+#endif
 
-    #if __GNUC__
-    std::cout << "  triqui/0.1: __GNUC__" << __GNUC__<< "\n";
-    #endif
+#if __GNUC__
+    std::cout << "  triqui/0.1: __GNUC__" << __GNUC__ << "\n";
+#endif
 
-    #if __GNUC_MINOR__
-    std::cout << "  triqui/0.1: __GNUC_MINOR__" << __GNUC_MINOR__<< "\n";
-    #endif
+#if __GNUC_MINOR__
+    std::cout << "  triqui/0.1: __GNUC_MINOR__" << __GNUC_MINOR__ << "\n";
+#endif
 
-    #if __clang_major__
-    std::cout << "  triqui/0.1: __clang_major__" << __clang_major__<< "\n";
-    #endif
+#if __clang_major__
+    std::cout << "  triqui/0.1: __clang_major__" << __clang_major__ << "\n";
+#endif
 
-    #if __clang_minor__
-    std::cout << "  triqui/0.1: __clang_minor__" << __clang_minor__<< "\n";
-    #endif
+#if __clang_minor__
+    std::cout << "  triqui/0.1: __clang_minor__" << __clang_minor__ << "\n";
+#endif
 
-    #if __apple_build_version__
-    std::cout << "  triqui/0.1: __apple_build_version__" << __apple_build_version__<< "\n";
-    #endif
+#if __apple_build_version__
+    std::cout << "  triqui/0.1: __apple_build_version__" << __apple_build_version__ << "\n";
+#endif
 
     // SUBSYSTEMS
 
-    #if __MSYS__
-    std::cout << "  triqui/0.1: __MSYS__" << __MSYS__<< "\n";
-    #endif
+#if __MSYS__
+    std::cout << "  triqui/0.1: __MSYS__" << __MSYS__ << "\n";
+#endif
 
-    #if __MINGW32__
-    std::cout << "  triqui/0.1: __MINGW32__" << __MINGW32__<< "\n";
-    #endif
+#if __MINGW32__
+    std::cout << "  triqui/0.1: __MINGW32__" << __MINGW32__ << "\n";
+#endif
 
-    #if __MINGW64__
-    std::cout << "  triqui/0.1: __MINGW64__" << __MINGW64__<< "\n";
-    #endif
+#if __MINGW64__
+    std::cout << "  triqui/0.1: __MINGW64__" << __MINGW64__ << "\n";
+#endif
 
-    #if __CYGWIN__
-    std::cout << "  triqui/0.1: __CYGWIN__" << __CYGWIN__<< "\n";
-    #endif
+#if __CYGWIN__
+    std::cout << "  triqui/0.1: __CYGWIN__" << __CYGWIN__ << "\n";
+#endif
 }
 
-void triqui_print_vector(const std::vector<std::string> &strings) {
-    for(std::vector<std::string>::const_iterator it = strings.begin(); it != strings.end(); ++it) {
+void triqui_print_vector(const std::vector<std::string> &strings)
+{
+    for (std::vector<std::string>::const_iterator it = strings.begin(); it != strings.end(); ++it)
+    {
         std::cout << "triqui/0.1 " << *it << std::endl;
     }
 }
